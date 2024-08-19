87c3db5dccdd1a6f91fbed3fa886c9be43abc67b
damir <damirdamir692@gmail.com>
2024-08-19 19:20:24 +0500

    Last 3.0

--- js/script.js
+++ js/script.js
@@ -10,20 +10,23 @@ $(document).ready(function() {
         var pageWidth = $(window).width();
         var pageHeight = $(window).height();
 
+        // Смещение страницы (для учета прокрутки)
+        var scrollLeft = $(window).scrollLeft();
+
         // Позиционирование всплывающего окна
         var top = e.pageY - (tooltipHeight / 2);
         var left = e.pageX + 15; // Окно появляется справа от курсора
 
-        var arrowLeft = '-15px'; // Позиция треугольника по умолчанию
+        var arrowLeft = '-14px'; // Позиция треугольника по умолчанию
         var arrowRight = 'auto';
 
-        // Проверка границ экрана по горизонтали
-        if (left + tooltipWidth > pageWidth) {
+        // Проверка границ экрана по горизонтали с учетом прокрутки
+        if (left + tooltipWidth > pageWidth + scrollLeft) {
             left = e.pageX - tooltipWidth - 15; // Окно перемещается слева от курсора
 
             // Корректируем положение треугольника для отображения справа
             arrowLeft = 'auto';
-            arrowRight = '-15px';
+            arrowRight = '-14px';
         }
 
         // Проверка границ экрана по вертикали
@@ -34,13 +37,13 @@ $(document).ready(function() {
         }
 
         // Обновление позиции треугольника на уровне мышки
-        var arrowTop = e.pageY - top - 15; // "- 5" для коррекции положения треугольника по центру курсора
+        var arrowTop = e.pageY - top - 14; // "- 5" для коррекции положения треугольника по центру курсора
 
         tooltipArrow.css({
             top: arrowTop + 'px', // Устанавливаем треугольник на уровне мышки
             left: arrowLeft,
             right: arrowRight,
-            transform: arrowLeft === '-15px' ? 'translateY(-50%) rotate(0deg)' : 'translateY(-50%) rotate(180deg)' // Поворачиваем треугольник
+            transform: arrowLeft === '-14px' ? 'translateY(-50%) rotate(0deg)' : 'translateY(-50%) rotate(180deg)' // Поворачиваем треугольник
         });
 
         tooltip.css({ top: top + 'px', left: left + 'px' });
